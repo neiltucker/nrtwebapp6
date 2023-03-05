636afbf35b7ac16a4eaa216f9828fa4777d74a3b
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2023-03-05 18:45:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
-openai.api_key = os.environ.get('openaiapikey')
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 def training_data(data):
     return """Please predict text as postive or negative.
